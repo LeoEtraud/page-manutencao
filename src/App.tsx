2b8c4dc2df4bf84077f4df357c0aa0914673a5e6
Leonardo Duarte <leocc_etraud@outlook.com>
2023-03-08 11:01:08 -0300

    Página de manutenção

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import img from './assets/manutencao.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', maxWidth: '100%' }}>
+    <img src={img} alt="Minha Imagem" style={{ width: '60%'}}/>
     </div>
+
   );
-}
+} 
 
 export default App;
